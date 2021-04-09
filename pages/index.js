1c91d6e2dd0c1241685d5a5c76e35c906ed1e122
DhairyaSharma7120 <dhairyasharma7120@gmail.com>
2021-04-09 17:57:58 +0530

    entire app

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Link from "next/Link"
+import Link from "next/link"
 import Image from "next/image"
 export default function Home() {
   return (
